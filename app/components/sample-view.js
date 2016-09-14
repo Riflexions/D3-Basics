bf585b75ee5cc2fb2fe7b664a153d395a273d7ef
bharathtn <bharathtn12@gmail.com>
2016-09-14 16:38:41 +0530

    Simple Bar dynamic chart

--- app/components/sample-view.js
+++ app/components/sample-view.js
@@ -1,8 +1,19 @@
 import Ember from 'ember';
-
+var data = [4, 8, 15, 16, 23, 42, 56, 72, 99];
 export default Ember.Component.extend({
     didRender() {
-        d3.selectAll("p").style("color", "red");
+
+        d3.select(".chart")
+            .selectAll("div")
+            .data(data)
+            .enter().append("div")
+            .style("width", function (d) {
+                return d * 10 + "px";
+            })
+            .text(function (d) {
+                return d;
+            });
+
     }
 
 });
