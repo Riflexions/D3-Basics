5a0e06fc81b89d3c967bb63ea01b603a5d8ab7bd
bharathtn <bharathtn12@gmail.com>
2016-09-14 18:37:06 +0530

    final

--- app/components/sample-view.js
+++ app/components/sample-view.js
@@ -1,14 +1,17 @@
 import Ember from 'ember';
 var data = [4, 8, 15, 16, 23, 42, 56, 72, 99];
+var x = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, 420]);
+
 export default Ember.Component.extend({
     didRender() {
-
         d3.select(".chart")
             .selectAll("div")
             .data(data)
             .enter().append("div")
             .style("width", function (d) {
-                return d * 10 + "px";
+                return x(d) + "px";
             })
             .text(function (d) {
                 return d;
